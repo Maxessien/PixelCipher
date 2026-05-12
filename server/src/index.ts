a70f277bd0151f18849028d83377720003152c39
Maxessien <essienmax484@gmail.com>
2026-05-12 03:21:54 +0100

    Refactor server build process: simplify build script in package.json, update static file serving, and remove unused files

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,11 +1,8 @@
 import express, { type Request, type Response } from "express";
 import multer from "multer";
-import { dirname, join } from "path";
-import { fileURLToPath } from "url";
+import { join } from "path";
 import { decodeTextInImage, encodeTextInImage } from "./controllers/imageController.js";
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
 
 declare global {
     namespace Express{
@@ -15,22 +12,22 @@ declare global {
     }
 }
 
-const upload = multer({dest: "/tmp/uploads"}) // Use /tmp for serverless environments
+const upload = multer({dest: "uploads"})
 
 
 const app = express()
 app.use(express.json())
 
 
-app.use(express.static(join(__dirname, "public")));
+app.use(express.static(join(process.cwd(), "../../client/dist")));
+
+// Serve SPA entry file for direct browser access.
+app.get("/", async(req: Request, res: Response)=>res.sendFile(join(process.cwd(), "../../client/dist/index.html")))
 
 // Accept multipart image uploads for steganography operations.
 app.post("/encode", upload.single("image"), encodeTextInImage)
 app.post("/decode",  upload.single("image"), decodeTextInImage)
 
-
-// Serve SPA entry file for direct browser access.
-app.get("*", async(req: Request, res: Response)=>res.sendFile(join(__dirname, "public/index.html")))
 
 const port = Number(process.env.PORT) || 5050
 
@@ -38,4 +35,4 @@ console.log(port)
 
 app.listen(port, "0.0.0.0", ()=>console.log("Server started on Port", "127.0.0.1:" + port))
 
-export default app;
+export default app
